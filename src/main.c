f45db106de7ca529d69e5ee5d2f0a24d59fb8cb9
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-07-28 14:06:37 -0300

    :construction: Update the way each block of the grid is represented
    - Each block is a struct with the number of adjacent bombs, if it is a
      bomb or not and if it is visible or not (all that data could be
    written on a single byte, probably a good optimization)

--- src/main.c
+++ src/main.c
@@ -2,13 +2,20 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+
+typedef struct Block {
+    unsigned short int n_bombs;
+    unsigned short int is_bomb;
+    unsigned short int visible;
+} Block, *BlockPtr;
+
 typedef struct Grid {
-    unsigned short int *blocks;
+    BlockPtr blocks;
     int n_rows;
     int n_opened;
 } Grid, *GridPtr;
 
-void incrementAdjacent(unsigned short int *grid, int n_rows, int spot) {
+void incrementAdjacent(BlockPtr grid, int n_rows, int spot) {
     int possible_movements[] = {-1, 0, 1};
     int bomb_spot[] = {spot/n_rows, spot%n_rows};
 
@@ -19,9 +26,9 @@ void incrementAdjacent(unsigned short int *grid, int n_rows, int spot) {
             if ((current_row >= 0 && current_row < n_rows) &&
                 (current_col >= 0 && current_col < n_rows) &&
                 (current_row != bomb_spot[0] || current_col != bomb_spot[1]) &&
-                (grid[n_rows*current_row + current_col] != 9))
+                (grid[n_rows*current_row + current_col].is_bomb == 0))
             {
-                grid[n_rows*current_row + current_col]++;
+                grid[n_rows*current_row + current_col].n_bombs++;
             }
         }
     }
@@ -29,13 +36,13 @@ void incrementAdjacent(unsigned short int *grid, int n_rows, int spot) {
 
 void generateBlocks(GridPtr grid) {
     int grid_size = grid->n_rows * grid->n_rows;
-    grid->blocks = (unsigned short int *)calloc(grid_size, sizeof(unsigned int short));
+    grid->blocks = (BlockPtr)calloc(grid_size, sizeof(Block));
     int n_bombs = 0;
 
     while (n_bombs != 10) {
         int bomb_spot = rand() % grid_size;
-        if (grid->blocks[bomb_spot] < 9) {
-            grid->blocks[bomb_spot] = 9;
+        if (grid->blocks[bomb_spot].is_bomb == 0) {
+            grid->blocks[bomb_spot].is_bomb = 1;
             n_bombs++;
             incrementAdjacent(grid->blocks, grid->n_rows, bomb_spot);
         }
@@ -55,7 +62,11 @@ GridPtr initGrid(int n_rows, int n_mines) {
 void printGrid(GridPtr grid) {
     for (int i = 0; i < grid->n_rows; ++i) {
         for (int j = 0; j < grid->n_rows; ++j) {
-            printf("%d ", grid->blocks[grid->n_rows*i + j]);
+            int current_idx = grid->n_rows*i + j;
+            if (grid->blocks[current_idx].is_bomb) {
+                printf("X ");
+            } else 
+                printf("%d ", grid->blocks[grid->n_rows*i + j].n_bombs);
         }
         puts("");
     }
